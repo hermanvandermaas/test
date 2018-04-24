94047b3cdb6679e660e405edc99bc2098a068cd5
Herman van der Maas <herman@waywayway.nl>
2018-04-24 17:29:38 +0200

    Initialen commit

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -1,14 +1,51 @@
 package com.mycompany.myapp;
 
-import android.app.*;
 import android.os.*;
+import android.support.v7.app.*;
+import android.util.*;
+import com.google.android.gms.location.places.*;
+import com.google.android.gms.location.places.ui.*;
+import com.google.android.gms.maps.*;
 
-public class MainActivity extends Activity 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback
 {
+	GoogleMap gMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	@Override
+	public void onMapReady(GoogleMap googleMap)
+	{
+		gMap = googleMap;
+		createPlaceSearch();
+    }
+
+	private void createPlaceSearch()
+	{
+		PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+			getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+		// Zoom naar gekozen Place
+		autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener()
+			{
+				@Override
+				public void onPlaceSelected(Place place)
+				{
+					gMap.animateCamera(CameraUpdateFactory.newLatLngBounds(place.getViewport(), 0));
+					//Toast.makeText(context, "Place: " + place.getName(), Toast.LENGTH_SHORT).show();
+					//Log.i("HermLog", "Place: " + place.getName());
+				}
+
+				@Override
+				public void onError(Status status)
+				{
+					Log.i("Info", "PlaceSelectionListener error: " + status);
+				}
+			});
+	}
 }
